a14329d9c98707f11aec661141784da1e8291111
unknown <quocbao.job106204@gmail.com>
2025-11-22 02:48:45 +0700

    fix: middleware import wrong in rout

--- backend/http/route/main.go
+++ backend/http/route/main.go
@@ -8,7 +8,7 @@ import (
 
 func SetupRouter(ctrl *controller.Controller) *gin.Engine {
 	r := gin.Default()
-	middles, err := middlewares.middlewares.NewMiddlewares(ctrl)
+	middles, err := middlewares.NewMiddlewares(ctrl)
 	if err != nil {
 		panic(err)
 	}
